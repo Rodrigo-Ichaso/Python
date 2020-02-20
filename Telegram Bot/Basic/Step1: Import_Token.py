69eddd3d9718fcc2de0b80775e73f10538dce3e9
Rodrigo Ichaso <gustavo.ichaso@gmail.com>
2020-02-20 16:22:21 -0400

    Update Step1: Import_Token.py

--- Telegram Bot/Basic/Step1: Import_Token.py
+++ Telegram Bot/Basic/Step1: Import_Token.py
@@ -1,6 +1,9 @@
-##
+##For this case i will going to use the pip method
+##pip install pyTelegramBotAPI form this lib https://pypi.org/project/pyTelegramBotAPI/0.3.0/
 
 import telebot
 
-TOKEN = "<put in here your telegram token>"
-bot = telebot.TeleBot(TOKEN)
+TOKEN = '<put in here your telegram token string>'
+tbot = telebot.TeleBot(TOKEN)
+
+##for obtain a token, you need to read this link https://core.telegram.org/bots#6-botfather
