dafcb861e54a63e11d40201d43453d77249809a4
Rodrigo Ichaso <gustavo.ichaso@gmail.com>
2020-02-20 16:53:40 -0400

    Update Step2: Sending_a_message.py

--- Telegram Bot/Basic/Step2: Sending_a_message.py
+++ Telegram Bot/Basic/Step2: Sending_a_message.py
@@ -3,4 +3,4 @@ import telebot
 tbot = telebot.TeleBot('put in here your telegram token string')
 tbot.send_message(243432536, 'My First Telegram bot message')
 
-#Comand we use = tbot.send_message(chatid, message)
+#command used = tbot.send_message(chatid, message)
